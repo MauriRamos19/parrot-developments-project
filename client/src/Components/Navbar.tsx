5b440e22773600ff91f917eb8159c87f169c7c03
MauriRamos19 <68834179+MauriRamos19@users.noreply.github.com>
2022-08-25 23:59:11 -0600

    Changes in client

--- client/src/Components/Navbar.tsx
+++ client/src/Components/Navbar.tsx
@@ -3,18 +3,27 @@ import { Link, NavLink } from 'react-router-dom'
 
 export const Navbar = () => {
   return (
-    <nav className="flex justify-around items-center w-screen h-24 bg-gray-800 bg-opacity-25 text-white">
-       <ul className='flex w-11/12 text-center justify-between'>
-            <li>
-                <NavLink to='/'>Home</NavLink>
-            </li>
-            {/* <li>
+    <nav className="flex w-screen h-24 bg-gray-800 bg-opacity-25 text-white shadow-md">
+        <div className='flex m-auto text-center font-extrabold'>
+            <img className='w-16' src='https://res.cloudinary.com/dzv5rmys1/image/upload/v1655030700/puma_logo_flklvk.png'/>
+       </div>
+       <div className='flex m-auto text-center font-extrabold'>
+            <div className='px-12 py-10 h-24 hover:bg-white hover:bg-opacity-25'>
+                <a href='/'>Home</a>
+                <div></div>
+            </div>
+            <div className='px-12 py-10 h-24 hover:bg-white hover:bg-opacity-25'>
                 <a>Marketplace</a>
-            </li>
-            <li>
+                <div></div>
+            </div>
+            <div className='px-12 py-10 h-24 hover:bg-white hover:bg-opacity-25'>
                 <a>About</a>
-            </li> */}
-       </ul>
+                <div></div>
+            </div>
+       </div>
+       <div className='flex m-auto text-center font-extrabold'>
+
+       </div>
     </nav>
   )
 }
